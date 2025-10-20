c283a7343101610a8d2309988f7542a02b01070d
wellington <wellington.h.s.oliveira@hotmail.com>
2025-10-20 15:35:38 -0300

    primeiro commit

---
+++ src/main/java/br/com/bffAgendador/bff_agendador_tarefas/BffAgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.bffAgendador.bff_agendador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bffAgendador/bff_agendador_tarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bffAgendador.bff_agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
